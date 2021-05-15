f755243132e0182f4b1dc91e1b3b338e3a629e42
lucassantoss1701 <lucas.santoss1701@gmail.com>
2021-05-15 10:26:31 -0300

    Domain level implemented

---
+++ src/main/java/com/luizalabs/luizalabs/domain/Address.java
@@ -0,0 +1,85 @@
+package com.luizalabs.luizalabs.domain;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String road;
+    private String district;
+    private String cep;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id")
+    private City city;
+
+    public Address(){
+
+    }
+
+    public Address(Integer id, String road, String district, String cep, City city) {
+        this.id = id;
+        this.road = road;
+        this.district = district;
+        this.cep = cep;
+        this.city = city;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRoad() {
+        return road;
+    }
+
+    public void setRoad(String road) {
+        this.road = road;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(id, address.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/luizalabs/luizalabs/domain/State.java
@@ -0,0 +1,62 @@
+package com.luizalabs.luizalabs.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class State {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "state")
+    private List<City> cities = new ArrayList<>();
+
+    public State(){
+
+    }
+
+    public State(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<City> getCities() {
+        return cities;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        State state = (State) o;
+        return Objects.equals(id, state.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
